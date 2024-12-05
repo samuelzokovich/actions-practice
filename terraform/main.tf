271d0898068e95e6dc413e0b156a55f9d1d8669e
samuelzokovich <77066529+samuelzokovich@users.noreply.github.com>
2024-12-05 17:23:52 +0530

    Add unformatted main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+# Required providers
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider configuration
+provider "azurerm" {
+  features {}
+}
+
+# Variables (Optional)
+variable "resource_group_name" {
+  description  = "The name of the Resource Group"
+  type      = string
+  default  = "my-resource-group"
+}
+
+variable "location" {
+  description = "The location/region for the Resource Group"
+  type      = string
+  default   = "East US"
+}
+
+# Resource: Azure Resource Group
+resource "azurerm_resource_group" "rg" {
+  name   = var.resource_group_name
+  location = var.location
+}
+
+# Outputs
+output "resource_group_name" {
+  description = "The name of the created Resource Group"
+  value  = azurerm_resource_group.rg.name
+}
+
+output "resource_group_location" {
+  description = "The location of the created Resource Group"
+  value  = azurerm_resource_group.rg.location
+}
